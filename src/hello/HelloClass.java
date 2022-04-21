590e0201d5330bee084655309320053d631edf03
HyunCheol96 <khc_961021@naver.com>
2022-04-21 10:47:34 +0900

    class 수정. class2 생성

--- src/hello/HelloClass.java
+++ src/hello/HelloClass.java
@@ -4,6 +4,8 @@ public class HelloClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("see you github");
+		System.out.println("hello github");
 
 	}
 
---
+++ src/hello/HelloClass2.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class HelloClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello class 2");
+
+	}
+
+}
